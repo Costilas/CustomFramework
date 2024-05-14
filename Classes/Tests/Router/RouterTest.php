b74dd2503aaea0e628af18d6bce53bd75c9db091
Sergei Kuzmin <sergei.kuzmin@softgamings.com>
2024-05-14 22:13:24 +0200

    Setting up tests and test directories

--- Classes/Tests/Router/RouterTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PHPUnit\Framework\TestCase;
-
-class RouterTest extends TestCase
-{
-
-}
---
+++ tests/Core/CoreTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\Core;
+
+use PHPUnit\Framework\TestCase;
+
+class CoreTest extends TestCase
+{
+    public function testCore()
+    {
+        $this->assertTrue(true);
+    }
+}
